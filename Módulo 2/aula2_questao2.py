232c61e74edf0796cbf1a27c3038067d70e5060f
anapaulalopes93 <102062241+anapaulalopes93@users.noreply.github.com>
2024-06-07 15:05:12 -0300

    Add files via upload

---
+++ Módulo 2/aula2_questao2.py
@@ -0,0 +1,20 @@
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+frase = "o resultado é"
+n1 = 10
+n2 = 3.5
+soma = n1 + n2
+
+print(frase)
+print(type(frase))
+
+print(n1)
+print(type(n1))
+
+print(n2)
+print(type(n2))
+
+print(soma)
+print(type(soma))
