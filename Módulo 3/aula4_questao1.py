667704792819a32fe66ac4777752de780c84e72a
anapaulalopes93 <102062241+anapaulalopes93@users.noreply.github.com>
2024-06-10 11:45:15 -0300

    Add files via upload

---
+++ Módulo 3/aula4_questao1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+soma = num1 + num2
+
+if(soma % 2 == 0):
+    print("A soma dos números informados é par.")
+else:
+    print("A soma dos números informados é ímpar.")
